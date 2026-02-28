4ec2f788312708989777daa72d4242f59dddeff2
amish-koli <amishkoli10@gmail.com>
2026-02-28 15:08:37 +0530

    BFP #3

--- BASIC FRONTEND PROJECTS/#3/script.js
+++ BASIC FRONTEND PROJECTS/#3/script.js
@@ -4,11 +4,13 @@ let check=1
 btn.addEventListener("click",()=>{
     if(check==1){
     text.textContent="Friend";
+    text.style.color="green";
     btn.textContent="Remove friend";
     check=0;
     }
     else{
         text.textContent="Stranger";
+        text.style.color="red";
         btn.textContent="Add friend";
         check=1;
     }
