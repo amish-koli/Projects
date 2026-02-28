17d0ae759431795e095cbbb334f51c2f014dcc88
amish-koli <amishkoli10@gmail.com>
2026-02-28 12:15:52 +0530

    BFP #2

--- BASIC FRONTEND PROJECTS/#2/script.js
+++ BASIC FRONTEND PROJECTS/#2/script.js
@@ -28,7 +28,7 @@ let timeintervalid = null;
 let intervalId = null;
 
 const blockWidth = 30;
-const blockHeight = 30;
+const blockHeight =30;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
