592a105902714775317f1a65ce5cfbf875238c2d
amish-koli <amishkoli10@gmail.com>
2026-03-10 14:21:32 +0530

    Project 1

--- Cohort Projects/script.js
+++ Cohort Projects/script.js
@@ -1,5 +1,6 @@
 let elems=document.querySelectorAll(".elems")
 let fullElems=document.querySelectorAll(".fullElems")
+let backbtn=document.querySelectorAll(".back-btn")
 elems.forEach(function(elem){
     elem.addEventListener("click",function(){
         let fullElem=fullElems[elem.id]
