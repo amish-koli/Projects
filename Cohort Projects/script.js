36fd0f8e364da5075605e7582c81f02cc7a02c0c
amish-koli <amishkoli10@gmail.com>
2026-03-10 15:51:12 +0530

    Project 1

--- Cohort Projects/script.js
+++ Cohort Projects/script.js
@@ -1,9 +1,16 @@
 let elems=document.querySelectorAll(".elems")
 let fullElems=document.querySelectorAll(".fullElems")
-let backbtn=document.querySelectorAll(".back-btn")
+let backbtn=document.querySelectorAll(".fullElems .back-btn i")
 elems.forEach(function(elem){
     elem.addEventListener("click",function(){
         let fullElem=fullElems[elem.id]
         fullElem.style.display="block";
+    })
+})
+backbtn.forEach(function(elem){
+    elem.addEventListener("click",function(){
+        setTimeout(function(){
+            let fullElem=fullElems[elem.id]
+        fullElem.style.display="none";},300)
     })
 })
