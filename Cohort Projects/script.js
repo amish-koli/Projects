80a75a31606dc34b0eab123c72bf64c20a79b3a4
amish-koli <amishkoli10@gmail.com>
2026-03-11 17:59:32 +0530

    Project 1

--- Cohort Projects/script.js
+++ Cohort Projects/script.js
@@ -18,15 +18,65 @@ function OpenPages() {
     })
 }
 OpenPages();
-let form=document.querySelector(".task-form form");
-let taskinput=document.querySelector(".task-form form input");
-let textarea=document.querySelector(".task-form form input");
+function Todo(){
+let currenttask = []
 
+let savedTasks = JSON.parse(localStorage.getItem("tasks"))
+
+if(savedTasks){
+    currenttask = savedTasks
+}
+
+let form = document.querySelector(".task-form form")
+let taskinput = document.querySelector(".task-form form input")
+let textarea = document.querySelector(".task-form form textarea")
+let markimp = document.querySelector(".task-form form .task-checkbox-div input")
+let tasklist = document.querySelector(".task-list")
+
+renderTasks()
 form.addEventListener("submit",function(e){
     e.preventDefault();
-    console.log(e);
-    console.log(taskinput);
     
+    currenttask.push({
+        taskname:taskinput.value,
+        taskdetails:textarea.value,
+        mark:markimp.checked
+    })
+
+    renderTasks();
+
+    localStorage.setItem("tasks", JSON.stringify(currenttask))
 })
+function renderTasks(){
+    let sum=``
 
+    currenttask.forEach(
+        function(elem,idx){    
+        sum=sum+`<div class="task-item" id=${idx}>
+        <h3 class="task-title">${elem.taskname}${elem.mark ? `<span class="imp">*</span>` : ``}</h3>
+        <input type="checkbox" name="" id="task-itemcheck">
+        </div>`
+    })
+    
+    tasklist.innerHTML=sum;
+}
 
+tasklist.addEventListener("change", function(e){
+
+    if(e.target.type === "checkbox"){
+        console.log(e.target.parentElement.id);
+        
+
+        let taskId = Number(e.target.parentElement.id)
+
+        currenttask.splice(taskId,1)
+
+        renderTasks()
+        taskinput.value = "";
+        textarea.value = "";
+        markimp.checked = false;
+        localStorage.setItem("tasks", JSON.stringify(currenttask))
+    }
+})
+}
+Todo();
